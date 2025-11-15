a32174298021ad95cc9c4491346ad67e83279b32
Emilia <emiliaoana01@gmail.com>
2025-11-15 19:38:35 +0200

    Initial commit

---
+++ src/main/java/com/example/search_as_you_type_es/SearchAsYouTypeEsApplication.java
@@ -0,0 +1,13 @@
+package com.example.search_as_you_type_es;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchAsYouTypeEsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchAsYouTypeEsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/search_as_you_type_es/SearchAsYouTypeEsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.search_as_you_type_es;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchAsYouTypeEsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
