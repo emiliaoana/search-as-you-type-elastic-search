489202fff17689c126602c3ccf2b4ef824f679ce
Emilia <emiliaoana01@gmail.com>
2025-11-15 19:59:27 +0200

    created book model

---
+++ src/main/java/com/example/search_as_you_type_es/model/Book.java
@@ -0,0 +1,53 @@
+package com.example.search_as_you_type_es.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.CompletionField;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+import org.springframework.data.elasticsearch.core.suggest.Completion;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(indexName = "books")
+public class Book {
+
+    @Id
+    private String id;
+
+    @Field(type = FieldType.Text)
+    private String title;
+
+    @Field(type = FieldType.Text)
+    private String author;
+
+    @Field(type = FieldType.Keyword)
+    private String isbn;
+
+    @Field(type = FieldType.Text)
+    private String description;
+
+    @Field(type = FieldType.Keyword)
+    private String category;
+
+    @Field(type = FieldType.Double)
+    private Double price;
+
+    @CompletionField(maxInputLength = 100)
+    private Completion suggest;
+
+    public Book(String id, String title, String author, String isbn, String description, String category, Double price) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.description = description;
+        this.category = category;
+        this.price = price;
+        this.suggest = new Completion(new String[]{title, author, category});
+    }
+}
